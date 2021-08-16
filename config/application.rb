78704d4d9ca822d8568f90590bdee9898be937d7
Uzair <uzairali19b@gmail.com>
2021-08-17 03:04:14 +0500

    update user_feed

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TwitterClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    Rack::MiniProfiler.config.position = 'bottom-right'
 
     # Configuration for the application, engines, and railties goes here.
     #
